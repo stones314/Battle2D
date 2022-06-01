b61a8aebef219b371dee5f755e5e1a1c6a0db3c2
Steinar Nerhus <snerhus@gmail.com>
2022-06-01 16:28:14 +0200

    Fixed run script to use scripded build path

--- B2DServer/BuildScripts/run_win64.sh
+++ B2DServer/BuildScripts/run_win64.sh
@@ -1,3 +1,3 @@
 echo Run
-'C:\Users\Steinar\Repos\Battle2D\B2DServer\Build\B2DServer.exe' -batchmode -nographics -b2d-address 127.0.0.1 -b2d-port 50123 -b2d-store-dir 'C:\Users\Steinar\AppData\LocalLow\Nerhus\B2DServer\Players\'
+'C:\Users\Steinar\Repos\Battle2D\B2DServer\builds\win64\B2DServer.exe' -batchmode -nographics -b2d-address 127.0.0.1 -b2d-port 50123 -b2d-store-dir 'C:\Users\Steinar\AppData\LocalLow\Nerhus\B2DServer\Players\'
 echo Ended Build Process
