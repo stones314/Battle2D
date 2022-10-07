071b24209e21d299d5953f20075e6b4da76a0fd8
Steinar Nerhus <snerhus@gmail.com>
2022-10-07 22:47:05 +0200

    Trying out doxygen for design/documentation

---
+++ Doc/code/defs.h
@@ -0,0 +1,21 @@
+/*! \file defs.h
+    \brief Definition of terms and types
+    
+    Contains definitions of various terms used in the game
+*/
+
+/*!
+    \brief The Phases in a Round of the game.
+    \details 
+    
+*/
+typedef enum Phase {
+    Shopping,
+    Battle
+};
+
+/*!
+    \brief A Round in the game
+    \details The game is played over several rounds.
+*/
+typedef int Round;
